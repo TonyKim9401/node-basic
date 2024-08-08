1ed7756bddd34bb935dcc4b0716a922b3d901d24
ktony <k.tony9412@gmail.com>
2024-08-08 17:30:16 -0400

    Commenc: what is a callback function in JS
    A callback function created to handle asynchronous situations during loading a web pages.
    
    There are 3 ways to make a asynchronous function
    1. Using callback function
    2. Using Promis fuction
    3. Using async & await

--- basics/02/sync.js
+++ basics/02/sync.js
@@ -1,1 +1,21 @@
 // 순서대로 실행하기  (결과 비교: 02\results\sync.js)
+
+function A() {
+    console.log('A');
+}
+
+function B(callback) {
+    setTimeout(() => {
+        console.log('B');
+        callback();
+    }, 2000);
+}
+
+function C() {
+    console.log('C');
+}
+
+A();
+
+// C() becomes a callback function
+B(C);
