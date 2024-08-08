2f87b1a2b5a7410bdb213b85ecbd7748f031b9f8
ktony <k.tony9412@gmail.com>
2024-08-08 18:06:41 -0400

    Comment: what types of path module in JS
    1. path.join(param1, param2, ...)
    create a new path by combining the given params
    2. path.dirname(param)
    return only the path except for the file name
    3. path.basename(param)
    return the exact file name including extension name
    4. path.basename(param, 'extension name')
    return only the exact pure file name except for extension

--- basics/03/path.js
+++ basics/03/path.js
@@ -1,2 +1,20 @@
 // path 모듈 연습하기 ( 결과 비교 파일 : 03\results\path.js)
 
+
+const path = require("path");
+
+// join function
+const fullpath = path.join('some', 'work', 'ex.txt');
+console.log(fullpath);
+
+// extract only path - dirname
+const dir = path.dirname(fullpath);
+console.log(dir);
+
+// extract only fine name - basename
+const fn1 = path.basename(__filename);
+console.log(`the file name is ${fn1}`);
+
+// extract only fine name - basename, except for the extention name
+const fn2 = path.basename(__filename, '.js');
+console.log(`the pure file name is ${fn2}`);
