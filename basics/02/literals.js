48159ec5d445b1d5ecb7df814aa5b64313e7abb3
ktony <k.tony9412@gmail.com>
2024-08-08 01:42:40 -0400

    Comment: What is literal in JS?
    Surrounding string with two backtic, when writting numbers surround them using ${ n }

--- basics/02/literals.js
+++ basics/02/literals.js
@@ -1,7 +1,7 @@
 let num1 = 10;
 let num2 = 20;
 
-// 연결 연산자 사용
-console.log(num1 + "과 " + num2 + "를 더하면 " + (num1 + num2) + "입니다.");
-// 템플릿 리터럴 사용
-console.log(`${num1}과 ${num2}를 더하면 ${num1 + num2}입니다.`);
+// plus operator
+console.log(num1 + " plus " + num2 + " is " + (num1 + num2));
+// template literal
+console.log(`${num1} plus ${num2} is ${num1 + num2}`);
