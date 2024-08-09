84b74ef79b89943355a2c2d0bb26b68835c932ec
ktony <k.tony9412@gmail.com>
2024-08-09 02:02:37 -0400

    Comment: what is routing?
    Routing is a concept that performing ddestructing in the server.
    Even though users access to the same server and port, through routing the server can provide many information to users.

--- basics/04/server-4.js
+++ basics/04/server-4.js
@@ -1,2 +1,20 @@
 // HTML 페이지 서빙하기  (결과 비교 파일 : 04\results\server-4.js)
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const { url, method } = req;
+    res.setHeader("Content-type", "text/plain");
+    if (url === "/home" && method === "GET") {
+        res.write("HOME");
+        res.end();
+    } else if (url === "/about" && method === "GET"){
+        res.end("ABOUT");
+    } else {
+        res.end("NOT FOUND");
+    }
+});
+
+server.listen(3000, () => {
+    console.log("server is running");
+});
