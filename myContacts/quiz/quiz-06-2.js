a570353d4fb3ebb12ebb4ee763e99832c125435c
ktony <k.tony9412@gmail.com>
2024-08-10 01:40:12 -0400

    Comment: The answers of the quiz

--- myContacts/quiz/quiz-06-2.js
+++ myContacts/quiz/quiz-06-2.js
@@ -1,2 +1,14 @@
 // ‘/users’ 경로로 요청할 때 사용자의 이름을 받아서
 // ‘Hello, Kyunghee!’ 처럼 인사말을 반환하는 API를 작성해 보세요.
+
+
+const express = require("express");
+const app = express();
+
+app.get("/users", (req, res) => {
+    res.send('Hello, Tony!');
+}) 
+
+app.listen(3000, () => {
+    console.log('The server is running');
+})
