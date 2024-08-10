742304185351ef4776be39765fc04d15b6317a3c
ktony <k.tony9412@gmail.com>
2024-08-10 13:13:13 -0400

    Comment: how to modulize route in Express.js
    By implementing express().Router we can easily create an instance of router and developing router part more like objectively.

--- myContacts/app.js
+++ myContacts/app.js
@@ -1,34 +1,12 @@
 const express = require("express");
 const app = express();
+const router = require("./routes/contactRoutes");
 
 app.get("/", (req, res) => {
     res.send('Hello, Node!');
 })
 
-// get contacts
-app.get("/contacts", (req, res) => {
-    res.send('Contacts Page');    
-})
-
-// get a specific contact
-app.get("/contacts/:id", (req, res) => {
-    res.send(`View Contact for ID: ${req.params.id}`);
-})
-
-// create new contact
-app.post("/contacts", (req, res) => {
-    res.send('Create Contacts');
-})
-
-// update contact
-app.put("/contacts/:id", (req, res) => {
-    res.send(`Update Contact for ID : ${req.params.id}`);
-})
-
-// delete contact
-app.delete("/contacts/:id", (req, res) => {
-    res.send(`Delete Contact for ID : ${req.params.id}`);
-})
+app.use("/contact", router);
 
 app.listen(3000, () => {
     console.log('The server is running');
