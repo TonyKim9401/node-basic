ba17a1fdd6de2796bd56bcb08e9513ccf2ed587b
ktony <k.tony9412@gmail.com>
2024-08-10 01:26:53 -0400

    Comment: how to use HTTP methods in Express.js
    Express.js provides much easy way to use HTTP methods. Check this out.
    1. app.get()
    2. app.post()
    3. app.put()
    4. app.delete()
    When you want to specify an object's id you can simply add :id at the back side of the routing address
    ex) /contacts/:id

--- myContacts/app.js
+++ myContacts/app.js
@@ -5,8 +5,29 @@ app.get("/", (req, res) => {
     res.send('Hello, Node!');
 })
 
+// get contacts
 app.get("/contacts", (req, res) => {
     res.send('Contacts Page');    
+})
+
+// get a specific contact
+app.get("/contacts/:id", (req, res) => {
+    res.send(`View Contact for ID: ${req.params.id}`);
+})
+
+// create new contact
+app.post("/contacts", (req, res) => {
+    res.send('Create Contacts');
+})
+
+// update contact
+app.put("/contacts/:id", (req, res) => {
+    res.send(`Update Contact for ID : ${req.params.id}`);
+})
+
+// delete contact
+app.delete("/contacts/:id", (req, res) => {
+    res.send(`Delete Contact for ID : ${req.params.id}`);
 })
 
 app.listen(3000, () => {
