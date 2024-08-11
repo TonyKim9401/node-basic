57eeca371d36a6207c295905860712a18e42bfd9
ktony <k.tony9412@gmail.com>
2024-08-11 16:22:06 -0400

    Comment: what is a body parser
    When the server received a request from a client, body values need to be parsed following certains ways and body parser works as a middleware to help parse request values to parse easy.
    
    - json format parser
    app.use(express.json()
    
    - url parser
    app.use(express.urlencodeed ({ extended: true }))

--- myContacts/app.js
+++ myContacts/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send('Hello, Node!');
 })
 
+// body parser middleware register
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
 app.use("/contact", router);
 
 app.listen(3000, () => {
--- myContacts/routes/contactRoutes.js
+++ myContacts/routes/contactRoutes.js
@@ -3,15 +3,21 @@
 const express = require("express");
 const router = express.Router();
 
+
 router.route("/")
     .get((req, res) => {
-            res.send('Contacts Page');    
+        res.send('Contacts Page');    
     })
     .post((req, res) => {
-            res.send('Create Contacts');
+        console.log(req.body);
+        const { name, email, phone } = req.body;
+        if (!name || !email || !phone) {
+            return res.send("input required");
+        }
+        res.send('Create Contacts');
     });
 
-router.route("//:id")
+router.route("/:id")
     .get((req, res) => {
         res.send(`View Contact for ID: ${req.params.id}`);
     })
